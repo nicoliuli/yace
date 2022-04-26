a5c55403b24c633b62cd678f03d5c7e06d907dff
li.liu <li.liu@ilivedata.com>
2022-04-26 19:41:42 +0800

    1

--- TestDemo/src/main/java/BranchMark.java
+++ TestDemo/src/main/java/BranchMark.java
@@ -23,22 +23,19 @@ public class BranchMark {
         t.start();
 
         Cli cli = new Cli();
-        String s = cli.doPostJson("http://localhost:18000/api/v1/image/check?debug=abc", "{\"type\":\"1\",\"appId\":\"91000001\",\"cache\":\"1\",\"image\":\"https://scpic.chinaz.net/files/pic/pic9/202204/apic40322.jpg\",\"debug\":\"abc\"}");
-        System.out.println(s);
-
-        /*while (true) {
-            long start = System.currentTimeMillis();
-            callShellByExec("curl localhost:18000/api/v1/image/check?debug=abc -X POST -d '{\"type\":\"1\",\"appId\":\"91000001\",\"cache\":\"1\",\"image\":\"https://scpic.chinaz.net/files/pic/pic9/202204/apic40322.jpg\",\"debug\":\"abc\"}' -H  \"X-AppId:91000001\" -H \"Content-Type: application/json;charset=UTF-8\"");
-            System.out.println(System.currentTimeMillis() - start);
-        }*/
+        while (true) {
+            String s = cli.doPostJson("http://localhost:18000/api/v1/image/check?debug=abc", "{\"type\":\"1\",\"appId\":\"91000001\",\"cache\":\"1\",\"image\":\"https://scpic.chinaz.net/files/pic/pic9/202204/apic40322.jpg\",\"debug\":\"abc\"}");
+            System.out.println(s);
+            i++;
+        }
     }
 
-    public static void callShellByExec(String shellString) {
+    /*public static void callShellByExec(String shellString) {
         try {
             Process process = Runtime.getRuntime().exec(shellString);
             i++;
         } catch (Throwable e) {
 
         }
-    }
+    }*/
 }
